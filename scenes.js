1830e5f2c500bb739c0e6eba357b759771bfcb08
WilliamLiu-1997 <77861330+WilliamLiu-1997@users.noreply.github.com>
2021-08-27 13:40:22 +1000

    Update scenes.js

--- scenes.js
+++ scenes.js
@@ -1229,6 +1229,7 @@ function select_recovery() {
     cut_obj = []
     line.clear();
     draw_line = [];
+    $("#alert_line").html("")
     if (controls !== undefined) {
         controls.target = false;
         controls.rotateSpeed = 1.3;
@@ -1242,7 +1243,6 @@ function select_recovery() {
 function draw(pointers, camera, cut_obj) {
     if (line.children.length > 3000) {
         $("#alert_line").html('<div id="line_alert" class="alert alert-warning fade in"><a href="#" class="close" data-dismiss="alert">&times;</a><strong><b>Warning!&nbsp;</b></strong>Drawed line is too long, recomend to split into short lines to improve performance!&nbsp;&nbsp;</div>');
-        setTimeout(function () { $("#alert_line").html("") },3000)
     } else {
         $("#alert_line").html("")
     }
